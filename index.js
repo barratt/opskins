ece10b4cb552571130b4c01a3dde5af6db628e05
Shaun Barratt <lecoffeestain@gmail.com>
2016-02-08 04:17:05 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,79 @@
+var request = require('request');
+var events = require('events');
+
+var userAPI = "https://opskins.com/api/user_api.php"
+
+function isOkay(err, res) {
+  if(err) {
+    return false;
+  }
+
+  if(res.statusCode == 200) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var OPSkins = function(apiKey) {
+  this.apiKey  = apiKey;
+  this.emitter =  new events.EventEmitter();
+
+  var self = this;
+
+  self._request("test", function(err, data) {
+    if (!err && data.success == 200) {
+      self.emitter.emit("ready");
+    } else {
+      self.emitter.emit("apikeyError", err)
+    }
+  })
+}
+
+OPSkins.prototype._request = function(action, data, callback) {
+
+  if (callback == null) {
+    callback = data // Optional data.
+  }
+
+  var url = userAPI + "?key=" + this.apiKey + "&request=" + action;
+  var keys = Object.keys(data);
+
+  for (var i = keys.length - 1; i >= 0; i--) {
+    url += "&" + keys[i] + "=" + data[keys[i]];
+  }
+
+  request(url, function(err, res, body) {
+    if (!isOkay(err, res)) {
+      return callback(err || new Error("Unknown error occured")); // Should probably improve this.
+    }
+    try {
+      var data = JSON.parse(body);
+      return callback(null, data);
+    } catch(Exception) {
+      return callback(new Error("Unable to parse reponse"));
+    }
+  });
+}
+
+OPSkins.prototype.on = function(event, callback) {
+  this.emitter.on(event, callback);
+}
+
+OPSkins.prototype.sellItem = function(assetid, amount, feature, callback) {
+  // feature and callback are optional.
+
+  if (feature == null) {
+    callback = function() {}
+  }
+
+  if (callback == null) {
+    callback = feature
+  }
+
+  this._request("SellItem", { assetid: assetid, amount: amount, featured: feature }, function(err, data) {
+    return callback(err, data.bot, data.bot_steamid, data.token, data.sale_id, data.tradeoffer_id);
+  });
+}
+
+module.exports = OPSkins
---
+++ readme.md
@@ -0,0 +1,31 @@
+#node-opskins
+### A node module to help automate opskins.com
+
+### Example
+
+```
+var opClient 	 = new OPSkins(APIKEY) // API Key see https://opskins.com/?loc=store_account
+
+opClient.on('ready', function() {
+	console.log("OPSkins API key works");
+});
+
+opClient.on('apikeyError', function(err) {
+	console.log("An error occured using API key " + err);
+})
+
+```
+
+### Todo:
+
+This isn't going to be a very maintained project unless it gains popularity, this was solely created for me to sell items on OPSkins without having to repeat lots of code.
+
+- GetActiveSales
+- GetStaleSales
+- ResendTrade
+- GetOP
+- Cashout
+- BumpItem
+- EditItem
+- ~SellItem~
+- GetPriceHistory
